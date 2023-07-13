eb4ad277c4734dd699585b15cb1b2296245460de
nalam <nalam@omnivex.com>
2023-07-13 12:23:55 +0500

    display platform icons

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -2,10 +2,17 @@ import { useEffect, useState } from "react";
 import apiClient from "../services/api-client";
 import { CanceledError } from "axios";
 
+export interface Platform {
+    id: number;
+    name: string;
+    slug: string;
+}
+
 export interface Game {
     id: number;
     name: string;
     background_image: string;
+    parent_platforms: {platform:Platform}[];
   }
   
   interface FetchGamesResponse {
@@ -31,7 +38,7 @@ const useGames = () => {
         });
 
         return () => controller.abort();
-    });
+    }, []);
 
     return {games, error};
 }
